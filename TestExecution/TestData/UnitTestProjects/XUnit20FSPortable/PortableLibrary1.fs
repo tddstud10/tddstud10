7117640008f60209e15cd860e2616ac67263ba8c
Partho P. Das <parthopdas@live.com>
2015-05-15 17:06:21 +1000

    test data for unit test platform

---
+++ TestExecution/TestData/UnitTestProjects/XUnit20FSPortable/PortableLibrary1.fs
@@ -0,0 +1,17 @@
+﻿module XUnit20FSPortable.UnitTests
+
+open Xunit
+
+[<Theory>]
+[<InlineData(1)>]
+[<InlineData(2)>]
+let ``Theory Tests`` input =
+    Assert.Equal(1, input)
+
+[<Fact>]
+let ``Fact Test 1`` () =
+    Assert.Equal(1, 1)
+
+[<Fact>]
+let ``Fact Test 2`` () =
+    Assert.Equal(1, 2)
