7daeaf6bfb9d40b3ad5ab8fca454c13fb3d7032e
Partho P. Das <parthopdas@live.com>
2015-07-17 20:57:45 +0700

    simplified project

--- incr/package/PkgCmdId.cs
+++ incr/package/PkgCmdId.cs
@@ -8,23 +8,17 @@ PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 
 ***************************************************************************/
 
-using System;
 
 namespace Microsoft.Samples.VisualStudio.IDE.ToolWindow
 {
-	/// <summary>
-	/// This class is used to expose the list of the IDs of the commands implemented
-	/// by the client package. This list of IDs must match the set of IDs defined inside the
-	/// BUTTONS section of the CTC file.
-	/// </summary>
-	static class PkgCmdId
-	{
-		// Define the list a set of public static members.
-		public const int cmdidPersistedWindow =    0x2001;
-		public const int cmdidUiEventsWindow =     0x2002;
-		public const int cmdidRefreshWindowsList = 0x2003;
-
-		// Define the list of menus (these include toolbars)
-		public const int IDM_MyToolbar =           0x0101;
-	}
+    /// <summary>
+    /// This class is used to expose the list of the IDs of the commands implemented
+    /// by the client package. This list of IDs must match the set of IDs defined inside the
+    /// BUTTONS section of the CTC file.
+    /// </summary>
+    static class PkgCmdId
+    {
+        // Define the list a set of public static members.
+        public const int cmdidUiEventsWindow = 0x2002;
+    }
 }
