0eeed39ce16b72ab647803d1c48da9ad202d494a
Partho P. Das <parthopdas@live.com>
2015-07-17 21:08:25 +0700

    Stripping further

--- incr/package/DynamicWindowWPFControl.xaml.cs
+++ incr/package/DynamicWindowWPFControl.xaml.cs
@@ -25,51 +25,14 @@ using System.Globalization;
 
 namespace Microsoft.Samples.VisualStudio.IDE.ToolWindow
 {
-    /// <summary>
-    /// Interaction logic for DynamicWindowWPFControl.xaml
-    /// </summary>
-    public partial class DynamicWindowWPFControl : UserControl
-    {
-        public DynamicWindowWPFControl()
-        {
-            InitializeComponent();
-        }
-
-        private WindowStatus currentState = null;
-		/// <summary>
-		/// This is the object that will keep track of the state of the IVsWindowFrame
-		/// that is hosting this control. The pane should set this property once
-		/// the frame is created to enable us to stay up to date.
-		/// </summary>
-		public WindowStatus CurrentState
+	/// <summary>
+	/// Interaction logic for DynamicWindowWPFControl.xaml
+	/// </summary>
+	public partial class DynamicWindowWPFControl : UserControl
+	{
+		public DynamicWindowWPFControl()
 		{
-			get { return currentState; }
-			set
-			{
-				if (value == null)
-					throw new ArgumentNullException("value");
-				currentState = value;
-				// Subscribe to the change notification so we can update our UI
-				currentState.StatusChange += new EventHandler<EventArgs>(this.RefreshValues);
-				// Update the display now
-				this.RefreshValues(this, null);
-			}
+			InitializeComponent();
 		}
-
-		/// <summary>
-		/// This method is the call back for state changes events
-		/// </summary>
-		/// <param name="sender">Event senders</param>
-		/// <param name="arguments">Event arguments</param>
-		private void RefreshValues(object sender, EventArgs arguments)
-		{
-            this.xText.Text = currentState.X.ToString(CultureInfo.CurrentCulture);
-            this.yText.Text = currentState.Y.ToString(CultureInfo.CurrentCulture);
-            this.widthText.Text = currentState.Width.ToString(CultureInfo.CurrentCulture);
-            this.heightText.Text = currentState.Height.ToString(CultureInfo.CurrentCulture);
-			this.dockedCheckBox.IsChecked = currentState.IsDockable;
-            this.InvalidateVisual();
-		}
-
-    }
+	}
 }
--- incr/package/PkgCmdId.cs
+++ incr/package/PkgCmdId.cs
@@ -1,24 +1,7 @@
-/***************************************************************************
-
-Copyright (c) Microsoft Corporation. All rights reserved.
-THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-
-***************************************************************************/
-
-
 namespace Microsoft.Samples.VisualStudio.IDE.ToolWindow
 {
-    /// <summary>
-    /// This class is used to expose the list of the IDs of the commands implemented
-    /// by the client package. This list of IDs must match the set of IDs defined inside the
-    /// BUTTONS section of the CTC file.
-    /// </summary>
     static class PkgCmdId
     {
-        // Define the list a set of public static members.
         public const int cmdidUiEventsWindow = 0x2002;
     }
 }
