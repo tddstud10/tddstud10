b72dc15f30c11b2508e2763bb4822cac5105c76c
jyotima@microsoft.com <jyotima@microsoft.com>
2016-08-05 00:32:52 -0700

    Addition solution isolation to enable/disable studio

--- Hosts/VS/TddStudioPackage/Settings.cs
+++ Hosts/VS/TddStudioPackage/Settings.cs
@@ -1,4 +1,6 @@
 ﻿
+using EnvDTE;
+
 namespace R4nd0mApps.TddStud10.Hosts.VS
 {
     using System.ComponentModel.Composition;
@@ -9,29 +11,38 @@ namespace R4nd0mApps.TddStud10.Hosts.VS
     [Export(typeof(Settings))]
     public class Settings
     {
+        private readonly SVsServiceProvider _vsServiceProvider;
         public const string IsTddStudioEnabled = "IsTddStudioEnabled";
-        private const string CollectionPath = "TddStudio";
+        private const string CollectionPathPrefix = "TddStudio";
         private readonly WritableSettingsStore _writableSettingsStore;
 
         [ImportingConstructor]
         public Settings(SVsServiceProvider vsServiceProvider)
         {
+            _vsServiceProvider = vsServiceProvider;
             var shellSettingsManager = new ShellSettingsManager(vsServiceProvider);
             _writableSettingsStore = shellSettingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
-            if (!_writableSettingsStore.CollectionExists(CollectionPath))
+            if (!_writableSettingsStore.CollectionExists(GetCollectionPath()))
             {
-                _writableSettingsStore.CreateCollection(CollectionPath);
+                _writableSettingsStore.CreateCollection(GetCollectionPath());
             }
         }
 
         public void SetSetting(string settingName, bool settingValue)
         {
-            _writableSettingsStore.SetBoolean(CollectionPath, settingName, settingValue);
+            _writableSettingsStore.SetBoolean(GetCollectionPath(), settingName, settingValue);
         }
 
         public bool GetSetting(string settingName)
         {
-            return _writableSettingsStore.GetBoolean(CollectionPath, settingName, true);
+            return _writableSettingsStore.GetBoolean(GetCollectionPath(), settingName, true);
+        }
+
+        private string GetCollectionPath()
+        {
+            var dte = (DTE)_vsServiceProvider.GetService(typeof(DTE));
+            var solutionName = System.IO.Path.GetFileNameWithoutExtension(dte.Solution.FullName);
+            return string.Format("{0}/{1}", CollectionPathPrefix, solutionName);
         }
     }
 }
