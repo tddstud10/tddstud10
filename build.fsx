e85f0bca255f8f78aa1cc5e44875be0cedad1b50
Partho P. Das <this@parthopdas.com>
2016-08-18 15:29:22 +1000

    Publishing nuget package for consumption by vscode

--- build.fsx
+++ build.fsx
@@ -3,17 +3,18 @@
 
 open Fake
 open Fake.Testing
+open System
 open System.IO
 
 // Directories
-let buildDir  = "./build/"
-let testDir  = "./build/"
+let buildDir  = @".\build\"
+let testDir  = @".\build\"
 
 // Filesets
 let solutionFile = "TddStud10.sln"
 
 // version info
-let version = "0.1"  // or retrieve from CI server
+let version = if buildServer = BuildServer.AppVeyor then AppVeyor.AppVeyorEnvironment.BuildNumber else "1.0.0.0"
 
 // Targets
 Target "Clean" (fun _ ->
@@ -57,12 +58,39 @@ Target "ContractTests"
         fun () -> traceImportant "Not required to run ContractTests on VS2013 boxes for simplifying test matrix.")
 
 
+Target "Package" (fun _ ->
+    let buildDirRel = sprintf @"..\build\%s"
+    let exclusions = 
+        "*.pdb;*.nupkg;*.nupkg.zip;*.vsix;Microsoft.VisualStudio.*;*.*Tests.dll;*.xml;*.lastcodeanalysissucceeded;approval*;xunit.*;envdte*;galasoft.*"
+        |> fun e -> e.Split([|';'|])
+        |> Array.map buildDirRel
+        |> fun es -> String.Join(";", es)
+
+    "TddStud10.nuspec"
+    |> NuGet (fun p -> 
+        { p with               
+            Authors = [ "The TddStud10 Team" ]
+            Project = "TddStud10.Core"
+            Description = "Core TddStud10 Runtime & Libraries"
+            Version = version
+            Files = [ buildDirRel "*.*", Some "bin", Some exclusions
+                      buildDirRel "amd64\*.*", Some @"bin\amd64", None
+                      buildDirRel "x86\*.*", Some @"bin\x86", None ]
+            OutputPath = buildDir })
+)
+
+Target "Publish" (fun _ ->
+    !! "build\*.nupkg"
+    |> AppVeyor.PushArtifacts
+)
+
 "Clean" ?=> "Build"
 "Clean" ==> "Rebuild" 
 "Build" ==> "Rebuild" 
 "Build" ?=> "UnitTests" ==> "Test"
 "Build" ?=> "ContractTests" ==> "Test"
 "Rebuild" ==> "Test"
+"Test" ==> "Package" ==> "Publish"
 
 // start build
 RunTargetOrDefault "Test"
