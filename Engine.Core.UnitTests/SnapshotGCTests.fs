1db85038b293fbe960aea8fccd1c9ef74503beaa
Partho P. Das <this@parthopdas.com>
2016-12-13 10:11:54 +1100

    tdd-ed SnapshotGC

---
+++ Engine.Core.UnitTests/SnapshotGCTests.fs
@@ -0,0 +1,88 @@
+﻿module R4nd0mApps.TddStud10.Engine.Core.SnapshotGCTests
+
+open FsUnit.Xunit
+open R4nd0mApps.TddStud10.Common.Domain
+open System.IO
+open System.Threading.Tasks
+open global.Xunit
+
+let (</>) a b = Path.Combine(a, b)
+
+let runTest t : Task = 
+    async { 
+        let rootDir = Path.GetTempPath() </> Path.GetRandomFileName()
+        Directory.CreateDirectory(rootDir) |> ignore
+        do! t rootDir
+        Directory.Delete(rootDir, true)
+    }
+    |> Async.StartAsTask :> _
+
+[<Fact>]
+let ``Should delete unmarked folder``() = 
+    runTest <| fun rootDir -> 
+        async { 
+            Directory.CreateDirectory(rootDir </> "A") |> ignore
+            let! garbage = SnapshotGC.collect (FilePath rootDir)
+            garbage
+            |> Seq.sort
+            |> Seq.toList
+            |> should matchList [ rootDir </> "A" ]
+            garbage
+            |> Seq.forall (Directory.Exists >> not)
+            |> should be True
+        }
+
+[<Fact>]
+let ``Should not delete fresh folder``() = 
+    runTest <| fun rootDir -> 
+        async { 
+            Directory.CreateDirectory(rootDir </> "A") |> ignore
+            SnapshotGC.unmark (FilePath <| (rootDir </> "A"))
+            let! garbage = SnapshotGC.collect (FilePath rootDir)
+            garbage
+            |> Seq.sort
+            |> Seq.toList
+            |> should be Empty
+            garbage
+            |> Seq.forall Directory.Exists
+            |> should be True
+        }
+
+[<Fact>]
+let ``Should delete stale folder``() = 
+    runTest <| fun rootDir -> 
+        async { 
+            Directory.CreateDirectory(rootDir </> "A") |> ignore
+            SnapshotGC.mark (FilePath <| (rootDir </> "A"))
+            let! garbage = SnapshotGC.collect (FilePath rootDir)
+            garbage
+            |> Seq.sort
+            |> Seq.toList
+            |> should matchList [ rootDir </> "A" ]
+            garbage
+            |> Seq.forall (Directory.Exists >> not)
+            |> should be True
+        }
+
+[<Fact>]
+let ``Should delete all stale folders``() = 
+    runTest <| fun rootDir -> 
+        async { 
+            Directory.CreateDirectory(rootDir </> "A") |> ignore
+            Directory.CreateDirectory(rootDir </> "B") |> ignore
+            SnapshotGC.mark (FilePath(rootDir </> "B"))
+            Directory.CreateDirectory(rootDir </> "C") |> ignore
+            SnapshotGC.unmark (FilePath(rootDir </> "C"))
+            let! garbage = SnapshotGC.collect (FilePath rootDir)
+            garbage
+            |> Seq.sort
+            |> Seq.toList
+            |> should matchList [ rootDir </> "A"
+                                  rootDir </> "B" ]
+            garbage
+            |> Seq.forall (Directory.Exists >> not)
+            |> should be True
+            [ rootDir </> "C" ]
+            |> Seq.forall Directory.Exists
+            |> should be True
+        }
---
+++ Engine.Core/SnapshotGC.fs
@@ -0,0 +1,31 @@
+﻿namespace R4nd0mApps.TddStud10.Engine.Core
+
+open R4nd0mApps.TddStud10.Common.Domain
+open System
+open System.IO
+
+module SnapshotGC = 
+    let private markerFile = "__tddstud10.gc.marker__"
+    let private stalingTime = TimeSpan.FromDays(1.0)
+    
+    let mark (FilePath sssr) = 
+        let marker = Path.Combine(sssr, markerFile)
+        File.WriteAllText(marker, "")
+        FileInfo(marker).LastWriteTimeUtc <- DateTime.UtcNow.Subtract(stalingTime)
+    
+    let unmark (FilePath sssr) = File.WriteAllText(Path.Combine(sssr, markerFile), "")
+    
+    let collect (FilePath ssr) = 
+        async { 
+            let isStale d = 
+                let marker = Path.Combine(d, markerFile)
+                not <| File.Exists(marker) || (DateTime.UtcNow - FileInfo(marker).LastWriteTimeUtc >= stalingTime)
+            
+            let garbage = 
+                Directory.EnumerateDirectories(ssr, "*", SearchOption.TopDirectoryOnly)
+                |> Seq.filter isStale
+                |> Seq.toList
+            
+            garbage |> Seq.iter (fun d -> Common.safeExec (fun () -> Directory.Delete(d, true)))
+            return garbage
+        }
