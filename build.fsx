17a9169b96c08aaf29fb4e7962f64f241112bd9b
Partho P. Das <this@parthopdas.com>
2016-12-15 14:39:13 +1100

    Consolidating the Test component

--- build.fsx
+++ build.fsx
@@ -38,7 +38,7 @@ Target "Build" (fun _ ->
     !! solutionFile
     |> MSBuild buildDir "Build"
          [
-            "Configuration", "Release"
+            "Configuration", "Debug"
             "Platform", "Any CPU"
             "CreateVsixContainer", "true"
             "DeployExtension", "false"
