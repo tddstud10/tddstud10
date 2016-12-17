a769a96a0e32906bc59208aae0042ba2b50ee23a
Partho P. Das <this@parthopdas.com>
2016-12-17 14:51:49 +1100

    Consuming Common component (Feature #60)

--- Common/Logger/LoggerFactory.fs
+++
@@ -1,3 +0,0 @@
-module R4nd0mApps.TddStud10.Logger.LoggerFactory
-
-let logger : ILogger = XFactory.X "R4nd0mApps.TddStud10.Logger.WindowsLogger" (NullLogger() :> _)
--- Common/Logger/TelemetryClientFactory.fs
+++
@@ -1,4 +0,0 @@
-module R4nd0mApps.TddStud10.Logger.TelemetryClientFactory
-
-let telemetryClient : ITelemetryClient = 
-    XFactory.X "R4nd0mApps.TddStud10.Logger.WindowsTelemetryClient" (NullTelemetryClient() :> _)
